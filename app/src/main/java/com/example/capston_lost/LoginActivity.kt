1f89e2460226aa8e0c7dd0c56c8e285052e692d9
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-05-06 16:01:42 +0900

    login

--- app/src/main/java/com/example/capston_lost/LoginActivity.kt
+++ app/src/main/java/com/example/capston_lost/LoginActivity.kt
@@ -52,7 +52,7 @@ class LoginActivity : AppCompatActivity() {
 
     }
     private fun updateUI(user: FirebaseUser?) {
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, SuccessActivity::class.java)
         startActivity(intent)
         finish()
     }
