b3c40dc258929590dc9cc7874ec19cd8d717e0f0
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-05-06 15:49:01 +0900

    login

--- app/src/main/java/com/example/capston_lost/LoginActivity.kt
+++ app/src/main/java/com/example/capston_lost/LoginActivity.kt
@@ -1,20 +1,59 @@
 package com.example.capston_lost
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Firebase 초기화
+        FirebaseApp.initializeApp(this)
+
+        // Firebase 인증 인스턴스 생성
+        auth = Firebase.auth
+        val loginBtn = findViewById<Button>(R.id.loginButton)
+        loginBtn.setOnClickListener {
+            val id = findViewById<EditText>(R.id.edit_id).text.toString()
+            val password = findViewById<EditText>(R.id.edit_pw).text.toString()
+            auth.signInWithEmailAndPassword(id, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        updateUI(null)
+                    }
+                }
         }
+
+    }
+    private fun updateUI(user: FirebaseUser?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/capston_lost/MainActivity.kt
+++ app/src/main/java/com/example/capston_lost/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.capston_lost
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.capston_lost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.memberButton.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java )
+            startActivity(intent)  // intent 실행
+        }
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java )
+            startActivity(intent)  // intent 실행
         }
     }
 }
