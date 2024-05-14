21af8d5f0401692d9a3b586c4b857d4e078d137f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-05-14 15:51:10 +0900

    Searchpage - 탭 레이아웃 설정

---
+++ app/src/main/java/com/example/capston_lost/LostFragment.kt
@@ -0,0 +1,20 @@
+package com.example.capston_lost
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class LostFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lost, container, false)
+    }
+}
--- app/src/main/java/com/example/capston_lost/Searchpage.kt
+++ app/src/main/java/com/example/capston_lost/Searchpage.kt
@@ -5,16 +5,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class Searchpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_searchpage)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
         setContentView(R.layout.activity_searchpage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        val lostFragment: Fragment = LostFragment()
+        val findFragment: Fragment = FindFragment()
+
+        supportFragmentManager.beginTransaction().replace(R.id.main_view, findFragment).commit()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, findFragment).commit()
+                    }
+                    1 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.main_view, lostFragment).commit()
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
     }
 }
