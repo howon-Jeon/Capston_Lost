99271e9a78a8910925509fd49057d4636a6736b4
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-05-18 18:29:49 +0900

    로그아웃 기능 add

--- app/src/main/java/com/example/capston_lost/Mypage.kt
+++ app/src/main/java/com/example/capston_lost/Mypage.kt
@@ -1,21 +1,33 @@
 package com.example.capston_lost
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.capston_lost.databinding.ActivityMypageBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 // import com.example.myapplication.R
 
 class Mypage : AppCompatActivity() {
+    private lateinit var binding: ActivityMypageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_mypage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMypageBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener {// 로그아웃 기능 구현
+            Firebase.auth.signOut()
+            val intent = Intent(this, MainActivity::class.java )
+            startActivity(intent)  // intent 실행
         }
+
     }
+
+
+
 }
