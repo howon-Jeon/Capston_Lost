9f66820815e07fbba7a605862472069e8ad0ddf8
Ji1WonKim <wltpfqosem@gmail.com>
2024-05-24 19:45:25 +0900

    FindReportpage.kt?

--- app/src/main/java/com/example/capston_lost/FindReportpage.kt
+++ app/src/main/java/com/example/capston_lost/FindReportpage.kt
@@ -3,9 +3,13 @@ package com.example.capston_lost
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FindReportpage : AppCompatActivity() {
 
@@ -17,11 +21,42 @@ class FindReportpage : AppCompatActivity() {
     private lateinit var editTextRemarks: EditText
     private lateinit var submitButton: Button
     private lateinit var db: FirebaseFirestore
-
+    val fbdb = Firebase.firestore
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_reportpage)
 
+        val btn1=findViewById<Button>(R.id.textViewSubmit)
+        val title=findViewById<EditText>(R.id.editTextTitle)
+        val itemtype=findViewById<EditText>(R.id.editTextItemType)
+        btn1.setOnClickListener {
+            val atitle=title.text
+            val aitem= itemtype.text
+
+            val adata= hashMapOf(
+                "title" to aitem.toString()
+            )
+
+            fbdb.collection("findreport").document(atitle.toString()).set(adata)
+                .addOnSuccessListener {
+                    showToast("데이터가 성공적으로 추가되었습니다.")
+                }
+                .addOnFailureListener { e ->
+                    // 추가 실패 시 처리
+                    showToast("데이터 추가에 실패했습니다.")
+                }
+
+        }
+
+
+
+
+    }
+}
+/*
         // Firebase Firestore 초기화
         db = FirebaseFirestore.getInstance()
 
@@ -70,4 +105,4 @@ class FindReportpage : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-}
+}*/
