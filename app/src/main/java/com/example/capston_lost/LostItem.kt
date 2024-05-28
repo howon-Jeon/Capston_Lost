ac21f6bb6f64802e74f5edd5e6000229f7b1a907
Ji1WonKim <wltpfqosem@gmail.com>
2024-05-28 11:42:53 +0900

    글쓰기

---
+++ app/src/main/java/com/example/capston_lost/LostItem.kt
@@ -0,0 +1,9 @@
+package com.example.capston_lost
+
+data class LostItem(
+    val title: String? = "",
+    val itemType: String? = "",
+    val getDate: String? = "",
+    val location: String? = "",
+    val remarks: String? = ""
+)
