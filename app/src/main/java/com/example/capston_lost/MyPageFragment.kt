0eea35780e7a603ce1def311f99204fddcb6b9e5
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-05-28 20:05:38 +0900

    박다은1 merge + 로그아웃 add

--- app/src/main/java/com/example/capston_lost/MyPageFragment.kt
+++ app/src/main/java/com/example/capston_lost/MyPageFragment.kt
@@ -1,10 +1,14 @@
 package com.example.capston_lost
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MyPageFragment : Fragment() {
     override fun onCreateView(
@@ -12,5 +16,17 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.activity_mypage, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val logoutButton: Button = view.findViewById(R.id.logout) // 취소 버튼
+
+        logoutButton.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
