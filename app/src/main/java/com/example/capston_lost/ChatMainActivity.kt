517b2f07e0016cf602ef1611261878cdb5b0c2c5
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-06-01 01:20:14 +0900

    채팅구현중

--- app/src/main/java/com/example/capston_lost/ChatMainActivity.kt
+++ app/src/main/java/com/example/capston_lost/ChatMainActivity.kt
@@ -8,12 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import com.example.capston_lost.chatlist.ChatListFragment
 import com.example.capston_lost.databinding.ActivityChatMainBinding
 import com.example.capston_lost.userlist.UserFragment
 
 class ChatMainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityChatMainBinding
     private  val userFragment = UserFragment()
+    private  val chatListFragment = ChatListFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatMainBinding.inflate(layoutInflater)
@@ -21,26 +23,26 @@ class ChatMainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.back -> {
-                    return@setOnItemSelectedListener  true
-                }
-                R.id.chatroomList ->{
-                    return@setOnItemSelectedListener  true
-                }
 
                 R.id.userList -> {
                     replaceFragment(userFragment)
                     return@setOnItemSelectedListener  true
                 }
 
-
+                R.id.chatroomList ->{
+                    replaceFragment(chatListFragment)
+                    return@setOnItemSelectedListener  true
+                }
+                R.id.back -> {
+                    return@setOnItemSelectedListener  true
+                }
                 else ->{
                     return@setOnItemSelectedListener false
                 }
 
             }
         }
-
+        replaceFragment(userFragment)
     }
 
     private fun replaceFragment(fragment: Fragment) {
