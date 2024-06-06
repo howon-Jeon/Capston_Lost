afe0c6adbfc7087fab4c9e6b64de89f4311dd4db
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-06-06 16:30:27 +0900

    저장

--- app/src/main/java/com/example/capston_lost/ChatMainActivity.kt
+++ app/src/main/java/com/example/capston_lost/ChatMainActivity.kt
@@ -18,7 +18,7 @@ import com.example.capston_lost.mypage.myPageFragments
 import com.example.capston_lost.userlist.UserFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
+
 import com.google.firebase.ktx.Firebase
 
 class ChatMainActivity : AppCompatActivity() {
